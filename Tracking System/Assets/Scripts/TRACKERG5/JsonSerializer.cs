2f8863405cc068da808e2567a4d95d84e80c73dd
Javier Comas De Frutos <javiercomasdefrutos@gmail.com>
2024-04-24 11:37:03 +0200

    Update JsonSerializer.cs

--- Tracking System/Assets/Scripts/TRACKERG5/JsonSerializer.cs
+++ Tracking System/Assets/Scripts/TRACKERG5/JsonSerializer.cs
@@ -25,8 +25,7 @@ namespace TrackerG5
             fs.Seek(0, SeekOrigin.Begin);
             if (fs.Length <= 0)
             {
-                byte[] data = Encoding.UTF8.GetBytes("[");
-                fs.Write(data, 0, data.Length);
+                fs.WriteByte((byte)'[');
             }
             else
             {
@@ -48,8 +47,7 @@ namespace TrackerG5
             // Truncar el archivo para eliminar el último carácter
             fs.SetLength(fs.Position);
 
-            byte[] data = Encoding.UTF8.GetBytes("]");
-            fs.Write(data, 0, data.Length);
+            fs.WriteByte((byte)']');
         }
     }
 }
